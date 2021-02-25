26cc0827e10041f5e0e9654768fd16509ddd867c
Francisco Javier <fjavils200590@gmail.com>
2021-02-25 15:04:37 +0100

    get override Menu

--- FoodOrganizer/app/src/main/java/es/fron99/Foodorganize/Models/Menu.kt
+++ FoodOrganizer/app/src/main/java/es/fron99/Foodorganize/Models/Menu.kt
@@ -3,13 +3,12 @@ package es.fron99.Foodorganize.Models
 class Menu(var id: Int, var name:String, var smallDescription:String, var foods:ArrayList<Food>) {
 
     var timeToPrepare : Int = 0
-
-    init {
-
-        for (i in foods.indices){
-            timeToPrepare += foods[i].timeToPrepare
+        get() {
+            field = 0;
+            for (i in foods.indices){
+                field += foods[i].timeToPrepare
+            }
+            return field
         }
-
-    }
 
 }
